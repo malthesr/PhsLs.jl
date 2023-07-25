3735f075bde12d056f55bb807d3ce339a21271f2
Malthe Sebro Rasmussen <malthesebro@protonmail.com>
2023-07-25 11:22:12 +0200

    Inplace util functions

--- src/utils.jl
+++ src/utils.jl
@@ -2,18 +2,26 @@ module Utils
 
 using Transducers: foldxt, Map
 
-export norm, norm!, cnorm, allsame, outer, symouter, 
-    colsum, rowsum, sumdrop, parmapreduce
+export norm, norm!, cnorm, cnorm!, allsame, outer, symouter, 
+    colsum, colsum!, rowsum, rowsum!, sumdrop, parmapreduce
 
 allsame(x) = all(y -> y == first(x), x)
 
 norm(x; kwargs...) = x ./ sum(x; kwargs...)
-function norm!(x; kwargs...)
-    x[:] /= sum(x; kwargs...)
+function norm!(x)
+    x[:] /= sum(x)
 end
+
 function cnorm(x)
     c = sum(x)
     (c, x ./ c)
+end
+function cnorm!(x)
+    c = sum(x)
+    @inbounds for i in eachindex(x)
+        x[i] /= c
+    end
+    c
 end
 
 outer(f, x, y) = broadcast(f, x, transpose(y))
@@ -25,6 +33,17 @@ symouter(x, y) = symouter(*, x, y)
 sumdrop(x; dims) = dropdims(sum(x, dims=dims), dims=dims)
 colsum(x::AbstractMatrix{Float64})::Vector{Float64} = sumdrop(x, dims=1)
 rowsum(x::AbstractMatrix{Float64})::Vector{Float64} = sumdrop(x, dims=2)
+
+function colsum!(v::AbstractVector, m::AbstractMatrix)
+    @inbounds for (i, s) in zip(eachindex(v), map(sum, eachcol(m)))
+        v[i] = s
+    end
+end
+function rowsum!(v::AbstractVector, m::AbstractMatrix)
+    @inbounds for (i, s) in zip(eachindex(v), map(sum, eachrow(m)))
+        v[i] = s
+    end
+end
 
 parmapreduce(f, op, it) = foldxt(op, it |> Map(f))
 
